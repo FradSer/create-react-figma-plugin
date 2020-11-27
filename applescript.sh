0fac1eb39a6908504bf1d30023fb3afeca63173c
Frad LEE <fradser@gmail.com>
2020-11-27 16:13:17 +0800

    refactor: optimize project structure

--- applescript.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-osascript <<'EOF'
-tell application "Figma" to activate
-tell application "System Events" to tell process "Figma"
-    keystroke "p" using {command down, option down}
-end tell
-EOF
